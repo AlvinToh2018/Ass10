c762d15b2fecab8e946e92841ff740a6f5f04548
AlvinToh2018 <44521110+AlvinToh2018@users.noreply.github.com>
2019-04-20 15:01:46 +0800

    Add files via upload

---
+++ DS_10.R
@@ -0,0 +1,38 @@
+library(tm)
+
+## Load Data
+blogs_raw <- readLines(connect <- file("R/en_US.blogs.txt"), encoding = "UTF-8", skipNul = TRUE, warn = FALSE)
+close(connect)
+
+news_raw <- readLines(connect <- file("R/en_US.news.txt"), encoding = "UTF-8", skipNul = TRUE, warn = FALSE)
+close(connect)
+
+twitter_raw <- readLines(connect <- file("R/en_US.twitter.txt"), encoding = "UTF-8", skipNul = TRUE, warn = FALSE)
+close(connect)
+
+
+## merge into a single corpus a smaller set (10%) for training
+## set seed to always use the same sample across runs
+merged_train <- paste(sample(blogs_raw, size = length(blogs_raw) / 10), 
+                      sample(news_raw, size = length(news_raw) / 10), 
+                      sample(twitter_raw, size = length(twitter_raw) / 10))
+
+
+# Use tm package to clean the data instead of using RE, & convert to lowercase
+corpus <- VCorpus(VectorSource(merged_train))
+
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeWords, stopwords("en"))
+corpus <-tm_map(corpus, tolower)
+
+# remove profanity
+profanity <- read.csv("http://www.bannedwordlist.com/lists/swearWords.txt", header=FALSE)
+corpus <- tm_map(corpus, removeWords, profanity[,1]) 
+
+# Do Stemming to collapse different forms of similar words
+corpus <- tm_map(corpus, stemDocument)
+
+
+
